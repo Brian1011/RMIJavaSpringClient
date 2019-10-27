9e18987bf9f89025514df175cbc68bae92d813e7
Brian1011 <mutindabrian26@gmail.com>
2019-10-28 00:49:53 +0300

    progress

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+    @Bean
+    RmiProxyFactoryBean rmiProxy(){
+        RmiProxyFactoryBean bean = new RmiProxyFactoryBean();
+        bean.setServiceInterface(ClientRMI.class);
+        bean.setServiceUrl("rmi://localhost:1199/CabBookingService");
+
+        return bean;
+    }
+
+    public static void main(String[] args) throws RemoteException {
+        //Registry registry = LocateRegistry.getRegistry("localhost");
+        //Registry registra = LocateRegistry.getRegistry();
+
+        ClientRMI rmiService = SpringApplication.run(DemoApplication.class, args).getBean(ClientRMI.class);
+        System.out.println("################## CLIENT ################");
+        System.out.println(rmiService.bookCab("Nairobi West"));
     }
 
 }
